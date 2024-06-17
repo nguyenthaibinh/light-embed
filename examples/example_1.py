aa35dac9ff7ae99a9fdc453f913b0f3040c28ee9
nguyenthaibinh <binhnguyen@BinhUWSookM1Pro>
2024-06-17 11:50:13 +0100

    first commit

---
+++ examples/example_1.py
@@ -0,0 +1,57 @@
+import os
+from typing import List
+import light_embed
+from light_embed import TextEmbedding
+from dotenv import load_dotenv
+from timeit import default_timer as timer
+import argparse
+
+def main():
+	print(f"light_embed.__version__: {light_embed.__version__}")
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		"--model-name", type=str,
+		default="sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
+	)
+	args = parser.parse_args()
+	model_name = args.model_name
+
+	load_dotenv()
+
+	cache_dir = os.getenv("SENTENCE_TRANSFORMERS_HOME")
+	
+	embedding_model = TextEmbedding(
+		model_name_or_path=model_name,
+		cache_folder=cache_dir,
+		quantize=False,
+		device="cpu"
+	)
+	
+	print("embedding_model:", embedding_model)
+	
+	documents: List[str] = [
+		"This is a light-weight and fast python library for generating embeddings.",
+		"This is built to be faster and lighter than other embedding libraries e.g. Transformers, Sentence-Transformers, etc.",
+	]
+	
+	start_embed = timer()
+	embeddings = embedding_model.encode(
+		documents, output_value="sentence_embedding",
+		return_as_array=False,
+		return_as_list=True
+	)
+	elapsed_time = timer() - start_embed
+	
+	print("embeddings:\n", embeddings)
+	print("type(embeddings):", type(embeddings))
+
+	print("len(embeddings[0]):", len(embeddings[0]))
+	print("type(embeddings[0]):", type(embeddings[0]))
+	print("model_name:", model_name)
+	print(f"elapsed time: {elapsed_time:.2f}")
+
+	return None
+
+if __name__ == "__main__":
+	main()
